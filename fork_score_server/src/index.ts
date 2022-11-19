7a8f1e63ae5bb581f9f609bab03d51d8e27197fb
liuwllm <liu.wllm@gmail.com>
2022-11-19 10:46:03 -0500

    login page

--- fork_score_server/src/index.ts
+++ fork_score_server/src/index.ts
@@ -1,1 +1,84 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcrypt');
+const path = require("path");
+const bodyParser = require('bodyparser');
+const users = require('./data-source').db;
+
+import { db } from "./data-source"
+
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+db.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'./login')));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'./login/login.html'));
+});
+
+app.post('/register', async (req, res) =>{
+    try{
+        let foundUser = user.find((data) => req.body.email === data.email);
+        if (!foundUser) {
+
+            let hashPassword = await bcrypt.hash(req.body.password, 10);
+
+            let newUser = {
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            };
+            users.push(newUser);
+            console.log('User list', users);
+            
+            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
+        } else {
+            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (foundUser) {
+    
+            let submittedPass = req.body.password; 
+            let storedPass = foundUser.password; 
+    
+            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+            if (passwordMatch) {
+                let usrname = foundUser.username;
+                res.send(`<div align ='center'><h2>Login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+            } else {
+                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+            }
+        }
+        else {
+    
+            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+            await bcrypt.compare(req.body.password, fakePass);
+    
+            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+
+app.listen(3000, () => {
+    console.log(`server started at port ${3000}`);
+});
